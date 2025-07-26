1a3928edfd7c7848dc312236bb12a47ff9fa6af3
fahrulbhsn <fahrululumbahasan@gmail.com>
2025-07-26 20:00:58 +0700

    Notifikasi Daftar Aset

--- resources/views/aset/index.blade.php
+++ resources/views/aset/index.blade.php
@@ -38,6 +38,16 @@
     </div>
     <div class="card-body">
         
+        {{-- Pesan Sukses --}}
+         @if (session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif       
+        
         {{-- Form Pencarian --}}
         <div class="row mb-3">
             <div class="col-md-4">
